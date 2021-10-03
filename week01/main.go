7e799c338778f87162109f96d87ea8419e0c0ac3
Joshua <joshua@Joshuas-MacBook-Pro.local>
2021-10-04 01:01:38 +0530

    add week-1

---
+++ week01/main.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello World")
+	a := "Go"
+	b := 42
+	c := true
+	fmt.Printf("Printing %T!\n", a)
+	fmt.Printf("Printing %T!\n", b)
+	fmt.Printf("Printing %T!\n", c)
+
+}
